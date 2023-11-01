92f274a04127c6b5751baf1af8d4dc4ec1afe0d8
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-11-01 15:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+    document.addEventListener("DOMContentLoaded", function () {
+        const colorSelect = document.getElementById("colorSelect");
+        const removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+
+        removeButton.addEventListener("click", function () {
+            // Get the selected option
+            const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+            
+            // Check if an option is selected
+            if (selectedOption) {
+                // Remove the selected option
+                colorSelect.remove(selectedOption.index);
+            }
+        });
+    });
+
